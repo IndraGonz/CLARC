c8f7bcae8884e3e0bcc7e2f0d25cb478ca9b3920
IndraGonz <igonzalezojeda@g.harvard.edu>
2025-02-24 10:40:20 -0500

    Added PPanGGolin input option as part of revisions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='CLARC',
-    version='1.1.3',
+    version='1.1.4',
     packages=find_packages(),
     scripts=['scripts/acccog_blastn.sh'],
     entry_points={
